212effdc545d86cdbabe52953a7f943c9a7326ed
ABHISHEKSONU2664 <144856198+ABHISHEKSONU2664@users.noreply.github.com>
2023-09-19 12:35:57 -0400

    Update project1.py
    udpated the code with latest changes

--- project1.py
+++ project1.py
@@ -1,2 +1,10 @@
-print("Hi ABHISHEK"); y = 42 ; x = 42
-print(eval(f'{input(2+2)}'))
+print("2 + 2")
+
+import math
+radius = 5
+volume = 4/3 * math . pi * radius ** 3
+print("The volume of the sphere with radius 5 is : " , volume)
+
+print(eval(f'{input(2 + 2)}'))
+
+print(eval(f'{input(24 + 24)}'))
